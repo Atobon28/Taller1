c8f1b5f87c69e0271cb73485f5c6eacd3b990069
Ana Tobon <anita.tobon28@gmail.com>
2025-03-04 10:41:46 -0500

    desafio 1, 2 y ayudas

---
+++ index.js
@@ -0,0 +1,12 @@
+//DESAFIO 1
+//Inversión de un Arreglo: Dado el siguiente arreglo de números, devuélvelo en orden inverso usando Lodash.
+const numeros = [1, 2, 3, 4, 5];
+const ordenLoda = require('lodash');
+const numerosInvertidos = ordenLoda.reverse(numeros);
+console.log(numerosInvertidos);
+
+//DESAFIO 2
+//Elimina Valores Falsy: Dado un arreglo con valores mixtos, filtra los valores falsy (false, 0, "", null, undefined, NaN).
+const valores = [0, "Hola", false, 42, "", null, "Mundo", undefined, NaN, true];
+const filtrados = ordenLoda.compact(valores);
+console.log(filtrados); 
